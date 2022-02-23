fb2daa7b7a50ce9dd517f38225cc32ca0bf55ef4
sravs1407 <95563750+sravs1407@users.noreply.github.com>
2022-02-23 10:14:02 +0530

    Create r3.r

---
+++ r3.r
@@ -0,0 +1,17 @@
+max(1.2,3.4,-7.8)
+max(c(1.2,3.4,-7.8))
+min(1,2,3,4)
+mode(1)
+class(T)
+class(1+2i)
+x<- matrix(nrow=5,ncol=3,byrow=T,data=1:15)
+x[3,]
+x[,2]
+y<-NA
+is.na(y)
+x<-c(11,NA,13)
+mean(x)
+isTRUE(8<6)
+x<-5
+x<10 && x>2
+x[1]>2 & x[1]<5
